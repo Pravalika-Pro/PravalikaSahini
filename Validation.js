2dfd26ae055e39791c0e5a963783f01045bf29a7
Pravalika-Pro <102150143+Pravalika-Pro@users.noreply.github.com>
2022-08-23 10:41:40 +0530

    Update Validation.js

--- Validation.js
+++ Validation.js
@@ -8,8 +8,8 @@
 function validlogin() 
 {
   var a = document.getElementById("mail_id").value;
-  var b = "pravalika@gmail.com" || "demo@gmail.com" || "pravalika@yahoo.com" || "helloall@outlook.com" 
-  if (a==b)
+  var b = "pravalika@gmail.com" || "demo@gmail.com" || "spravalika@yahoo.com" || "helloall@outlook.com" 
+  if (a=="pravalika@gmail.com")
   {
     //passing user and account objects:
     aptrinsic("identify",
@@ -26,9 +26,32 @@ function validlogin()
     },
 {
 //Account Fields
-  "id":"IBM", //Required
+  "id":"IBM123", //Required
   "name":"International Business Machine",
   "Program": "Platinum" // flat custom attributes
+});
+    location.replace("WelcomePage.html");
+  }
+  else if (a=="spravalika@yahoo.com")
+  {
+      //passing user and account objects:
+    aptrinsic("identify",
+    {
+    //User Fields
+    "id": "PX:"+a, // Required for logged in app users
+    "email": a,
+    "firstName": "PravS",
+    "lastName": "Yahoo",
+    "signUpDate": 1522697426479, //unix time in ms
+    "plan" : "Premium", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+    "price" : 120.8,
+    "userHash": "" // optional transient for HMAC identification
+    },
+{
+//Account Fields
+  "id":"Amazon123", //Required
+  "name":"Amazon Global Services",
+  "Program": "Diamond" // flat custom attributes
 });
     location.replace("WelcomePage.html");
   }
@@ -53,6 +76,124 @@ function deleteAllCookies()
 {
   Session.Abandon();
   Response.Cookies.Clear();
+}
+
+function allowlogin(mail_id) 
+{
+    //var a = document.form.usermail.value;
+    var a = document.getElementById("usermail").value;
+    var b;
+    const myaccount = 
+    {
+        a1: "Aptrinsic PX",
+        a2: "Facebook",
+        a3: "Amazon",
+        a4: "Salesforce"
+
+    }
+    var c;
+
+
+    if (a != "") 
+    {
+        if (a == "sahini@gmail.com" || a == "pravalika@outlook.com" || a == "pravalika@yahoo.com" || a == "sahini@yahoo.com" || a == "testuser@gmail.com") 
+        {
+            alert("valid User");
+            b = a.substr(8, 5);
+            var id = b;
+
+            /*<!-- Aptrinsic Tag-->*/
+
+
+            if (a == 'sahini@gmail.com') {
+                aptrinsic('identify', {
+                        //User Fields
+                        "id": "0057F000001wxPcQAIMandiantUsecase", // Required for logged in app users
+                        "email": a,
+                        "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+
+                        // "userHash": hash.toString()// optional transient for HMAC identification
+                    },
+
+                    {
+                        //Account Fields
+                        "id": myaccount.a4, //Required
+                        "name": myaccount.a4,
+                        // flat custom attributes
+                    });
+            } else if (a == 'pravalika@outlook.com') {
+                aptrinsic('identify', {
+                        //User Fields
+                        "id": id, // Required for logged in app users
+                        "email": a,
+                        "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+
+                        // "userHash": hash.toString()// optional transient for HMAC identification
+                    },
+
+                    {
+                        //Account Fields
+                        "id": myaccount.a3, //Required
+                        "name": myaccount.a3,
+                        // flat custom attributes
+                    });
+            } else if (a == 'sahini@yahoo.com') {
+                aptrinsic('identify', {
+                        //User Fields
+                        "id": id, // Required for logged in app users
+                        "email": a,
+                        "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+
+                        // "userHash": hash.toString()// optional transient for HMAC identification
+                    },
+
+                    {
+                        //Account Fields
+                        "id": myaccount.a2, //Required
+                        "name": myaccount.a2,
+                        // flat custom attributes
+                    });
+            } else {
+                aptrinsic('identify', {
+                        //User Fields
+                        "id": id, // Required for logged in app users
+                        "email": a,
+                        "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+
+                        // "userHash": hash.toString()// optional transient for HMAC identification
+                    },
+
+                    {
+                        //Account Fields
+                        "id": myaccount.a1, //Required
+                        "name": myaccount.a1,
+                        // flat custom attributes
+                    });
+            }
+
+
+            // Full story
+            // This is an example script - don't forget to change it!
+            FS.identify(id, {
+                displayName: a,
+                email: a
+                // TODO: Add your own custom user variables here, details at
+                // https://help.fullstory.com/hc/en-us/articles/360020623294-FS-setUserVars-Recording-custom-user-data
+
+            });
+
+            alert("Logged in user id :" + b);
+
+            window.location = "https://pravalika-pro.github.io/PravalikaSahini/WelcomePage.html";
+            aptrinsic('track', 'User Login');
+
+        } else
+            alert("Invalid User,enter \"testuser@gmail.com\"");
+    } else
+        alert("enter \"testuser@gmail.com\"");
+
+
+    return b;
 }
 
 function openRadio()
