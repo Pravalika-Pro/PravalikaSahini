95ad390c51a1001aa6926f95aa6f94c03725ea0e
Pravalika-Pro <102150143+Pravalika-Pro@users.noreply.github.com>
2026-03-25 07:53:37 +0530

    Update Validation.js for kcb hide

--- Validation.js
+++ Validation.js
@@ -3,7 +3,14 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+      var config = {
+  kcAllowedFuncNames: ["disableKCB"]
+};
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-CYLRDPNTFF8V-2");
+
+function disableKCB() {
+  aptrinsic('kcb', 'hide');
+}
 
 
 function openMyKcb()
